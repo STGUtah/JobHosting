e7a3edc69bee455c15cb8d2a0509d8eaf488839c
Scott Knutti <sknutti@gmail.com>
2016-04-07 13:40:45 -0600

    Initial commit

---
+++ web/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:3000/'
+  },
+  plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Job Hosting',
+        hash: true,
+        inject: false,
+        template: path.join(__dirname, '/assets/index-template.ejs')
+      })
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /.ejs$/,
+      exclude: /index.html$/,
+      loader: 'ejs'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!sass-loader'
+    }]
+  }
+};
